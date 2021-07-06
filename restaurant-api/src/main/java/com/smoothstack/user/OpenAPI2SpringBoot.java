c72fd60df05ad51ef2bfb15961e41b4d43c2bfc8
jasonwittmuss <jason.wittmuss@smoothstack.com>
2021-07-06 13:32:52 -0500

    Testing for the build error

--- restaurant-api/src/main/java/com/smoothstack/user/OpenAPI2SpringBoot.java
+++ restaurant-api/src/main/java/com/smoothstack/user/OpenAPI2SpringBoot.java
@@ -6,15 +6,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.Serial;
-
-@SpringBootApplication
-@ComponentScan(basePackages = {"org.openapitools", "com.smoothstack.user.api", "com.smoothstack.user.configuration"})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EntityScan("com.database.ormlibrary")
 public class OpenAPI2SpringBoot implements CommandLineRunner {
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(OpenAPI2SpringBoot.class).run(args);
+    }
 
     @Override
     public void run(String... arg0) throws Exception {
@@ -23,12 +26,27 @@ public class OpenAPI2SpringBoot implements CommandLineRunner {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        new SpringApplication(OpenAPI2SpringBoot.class).run(args);
+    @Bean
+    public WebMvcConfigurer webConfigurer() {
+        return new WebMvcConfigurer() {
+            /*
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("Content-Type");
+            }
+            */
+        };
+    }
+
+    @Bean
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
     }
 
     static class ExitException extends RuntimeException implements ExitCodeGenerator {
-        @Serial
         private static final long serialVersionUID = 1L;
 
         @Override
@@ -36,24 +54,6 @@ public class OpenAPI2SpringBoot implements CommandLineRunner {
             return 10;
         }
 
-    }
-
-    @Bean
-    public WebMvcConfigurer webConfigurer() {
-        return new WebMvcConfigurer() {
-            /*@Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("Content-Type");
-            }*/
-        };
-    }
-
-    @Bean
-    public Module jsonNullableModule() {
-        return new JsonNullableModule();
     }
 
 }
--- restaurant-api/src/main/java/com/smoothstack/user/api/RestaurantApiController.java
+++ restaurant-api/src/main/java/com/smoothstack/user/api/RestaurantApiController.java
@@ -1,8 +1,13 @@
 package com.smoothstack.user.api;
 
+import com.smoothstack.user.model.Restaurant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.List;
 import java.util.Optional;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-06-30T22:10:31.533216200-06:00[America/Denver]")
 @Controller
@@ -21,4 +26,9 @@ public class RestaurantApiController implements RestaurantApi {
         return Optional.ofNullable(request);
     }
 
+    @Override
+    public ResponseEntity<List<Restaurant>> getFood(String search, String geolocation, String distance, String filterAllergens, String filterDietaryRestrictions, Integer stars, Integer page, Integer size) {
+        // return RestaurantApi.super.getFood(search, geolocation, distance, filterAllergens, filterDietaryRestrictions, stars, page, size);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
