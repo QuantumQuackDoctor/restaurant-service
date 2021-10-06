7d76e95b0277c569aca0fd62397dbf1cdd48df82
jasonwittmuss <jason.wittmuss@smoothstack.com>
2021-10-05 21:44:33 -0500

    Readme update

--- restaurant-api/src/main/java/com/smoothstack/user/api/ApiUtil.java
+++ restaurant-api/src/main/java/com/smoothstack/user/api/ApiUtil.java
@@ -9,6 +9,9 @@ public class ApiUtil {
     public static void setExampleResponse(NativeWebRequest req, String contentType, String example) {
         try {
             HttpServletResponse res = req.getNativeResponse(HttpServletResponse.class);
+            if (res == null) {
+              throw new RuntimeException();
+            }
             res.setCharacterEncoding("UTF-8");
             res.addHeader("Content-Type", contentType);
             res.getWriter().print(example);
