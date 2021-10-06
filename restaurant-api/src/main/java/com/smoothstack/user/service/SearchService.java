161ee3566eab1f794f8fab3267e385bdaa351ccb
jasonwittmuss <jason.wittmuss@smoothstack.com>
2021-10-05 21:06:53 -0500

    Health Check Changes

--- restaurant-api/src/main/java/com/smoothstack/user/service/SearchService.java
+++ restaurant-api/src/main/java/com/smoothstack/user/service/SearchService.java
@@ -3,7 +3,6 @@ package com.smoothstack.user.service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.database.ormlibrary.food.MenuItemEntity;
